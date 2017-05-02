9d528f0c7944ba93872b0cfc70a5d0ec18d5ab74
Massimo Nocentini <massimo.nocentini@gmail.com>
2017-05-02 11:57:59 +0200

    Create a ``src`` folder for sources.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,10 +19,7 @@
 #
 import os
 import sys
-sys.path.append(os.path.abspath('.'))
-sys.path.append(os.path.abspath('../'))
-sys.path.append(os.path.abspath('../../'))
-sys.path.append(os.path.abspath('../../muk/'))
+sys.path.insert(0, os.path.abspath('../../src/'))
 print(sys.path)
 
 
